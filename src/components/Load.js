dfeef69cf489aefb0a7dbf777e6aa702b2712cc6
viji <viji.rajhamohan@klinikeverywhere.com>
2023-02-17 23:52:28 +0530

    first commit

---
+++ src/components/Load.js
@@ -0,0 +1,91 @@
+import React from 'react'
+
+export default function Load() {
+  return (
+      <div class="table-responsive">
+          <table class="table table-bordered ">
+              <thead>
+                  <tr>
+                      <td colspan="7" className="text-center">
+                          <h5>Load</h5>
+                      </td>
+                  </tr>
+                  <tr>
+                      <th scope="col">Staff Member</th>
+                      <th scope="col">Monday</th>
+                      <th scope="col">Tuesday</th>
+                      <th scope="col">Wednesday</th>
+                      <th scope="col">Thursday</th>
+                      <th scope="col">Friday</th>
+                      <th scope="col">Total</th>
+                  </tr>
+              </thead>
+              <tbody>
+                  <tr>
+                      <th scope="row">X1</th>
+                      <td>Y</td>
+                      <td>Y</td>
+                      <td>Y</td>
+                      <td>Y</td>
+                      <td>Y</td>
+                      <td>YY</td>
+                  </tr>
+                  <tr>
+                      <th scope="row">X2</th>
+                      <td>Y</td>
+                      <td>Y</td>
+                      <td>Y</td>
+                      <td>Y</td>
+                      <td>Y</td>
+                      <td>YY</td>
+                  </tr>
+                  <tr>
+                      <th scope="row">X3</th>
+                      <td>Y</td>
+                      <td>Y</td>
+                      <td>Y</td>
+                      <td>Y</td>
+                      <td>Y</td>
+                      <td>YY</td>
+                  </tr>
+                  <tr>
+                      <th scope="row">X4</th>
+                      <td>Y</td>
+                      <td>Y</td>
+                      <td>Y</td>
+                      <td>Y</td>
+                      <td>Y</td>
+                      <td>YY</td>
+                  </tr>
+                  <tr>
+                      <th scope="row">X5</th>
+                      <td>Y</td>
+                      <td>Y</td>
+                      <td>Y</td>
+                      <td>Y</td>
+                      <td>Y</td>
+                      <td>YY</td>
+                  </tr>
+                  <tr>
+                      <th scope="row">X6</th>
+                      <td>Y</td>
+                      <td>Y</td>
+                      <td>Y</td>
+                      <td>Y</td>
+                      <td>Y</td>
+                      <td>YY</td>
+                  </tr>
+                  <tr>
+                      <th scope="row">X7</th>
+                      <td>Y</td>
+                      <td>Y</td>
+                      <td>Y</td>
+                      <td>Y</td>
+                      <td>Y</td>
+                      <td>YY</td>
+                  </tr>
+              </tbody>
+          </table>
+      </div>
+  )
+}
---
+++ src/components/Schedule.js
@@ -0,0 +1,227 @@
+import React from "react";
+
+export default function Schedule() {
+  return (
+    <div className="container aline-center table-responsive mx-auto">
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <td colspan="7" className="text-center">
+              <h5>Schedule</h5>
+            </td>
+          </tr>
+          <tr>
+            <th scope="col"></th>
+            <th scope="col">Monday</th>
+            <th scope="col">Tuesday</th>
+            <th scope="col">Wednesday</th>
+            <th scope="col">Thursday</th>
+            <th scope="col">Friday</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">Morning UpStairs </th>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none " /></td>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none " /></td>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none " /></td>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none " /></td>
+            <td></td>
+          </tr>
+          <tr>
+            <th scope="row">Morning Down Stairs </th>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td></td>
+          </tr>
+          <tr>
+            <th scope="row">Morning Parking Lot </th>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td></td>
+          </tr>
+          <tr>
+            <th scope="row"></th>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <th scope="row">Lunch A </th>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td></td>
+          </tr>
+          <tr>
+            <th scope="row">Lunch B </th>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td></td>
+          </tr>
+          <tr>
+            <th scope="row">Lunch C </th>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td></td>
+          </tr>
+          <tr>
+            <th scope="row">Lunch D </th>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td></td>
+          </tr>
+          <tr>
+            <th scope="row"></th>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <th scope="row">Afternoon UpStairs </th>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td></td>
+          </tr>
+          <tr>
+            <th scope="row">Afternoon Down Stairs </th>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td></td>
+          </tr>
+          <tr>
+            <th scope="row">Afternoon Parking Lot </th>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td><input type="text" placeholder="X" className="btn btn-outline-none "/></td>
+            <td></td>
+          </tr>
+
+          <tr>
+            <th scope="row"></th>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+        {/* <thead>
+          <tr>
+            <td colspan="7" className="text-center">
+              <h5>Load</h5>
+            </td>
+          </tr>
+          <tr>
+            <th scope="col">Staff Member</th>
+            <th scope="col">Monday</th>
+            <th scope="col">Tuesday</th>
+            <th scope="col">Wednesday</th>
+            <th scope="col">Thursday</th>
+            <th scope="col">Friday</th>
+            <th scope="col">Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">X1</th>
+            <td>Y</td>
+            <td>Y</td>
+            <td>Y</td>
+            <td>Y</td>
+            <td>Y</td>
+            <td>YY</td>
+          </tr>
+          <tr>
+            <th scope="row">X2</th>
+            <td>Y</td>
+            <td>Y</td>
+            <td>Y</td>
+            <td>Y</td>
+            <td>Y</td>
+            <td>YY</td>
+          </tr>
+          <tr>
+            <th scope="row">X3</th>
+            <td>Y</td>
+            <td>Y</td>
+            <td>Y</td>
+            <td>Y</td>
+            <td>Y</td>
+            <td>YY</td>
+          </tr>
+          <tr>
+            <th scope="row">X4</th>
+            <td>Y</td>
+            <td>Y</td>
+            <td>Y</td>
+            <td>Y</td>
+            <td>Y</td>
+            <td>YY</td>
+          </tr>
+          <tr>
+            <th scope="row">X5</th>
+            <td>Y</td>
+            <td>Y</td>
+            <td>Y</td>
+            <td>Y</td>
+            <td>Y</td>
+            <td>YY</td>
+          </tr>
+          <tr>
+            <th scope="row">X6</th>
+            <td>Y</td>
+            <td>Y</td>
+            <td>Y</td>
+            <td>Y</td>
+            <td>Y</td>
+            <td>YY</td>
+          </tr>
+          <tr>
+            <th scope="row">X7</th>
+            <td>Y</td>
+            <td>Y</td>
+            <td>Y</td>
+            <td>Y</td>
+            <td>Y</td>
+            <td>YY</td>
+          </tr>
+        </tbody>*/}
+      </table> 
+    </div>
+  );
+}
